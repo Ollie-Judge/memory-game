e457f476ff91ffb83a561b6c5fd0339ac95116a4
Ollie-Judge <ollie.judge@hotmail.com>
2023-06-28 12:31:30 +0100

    added a replay functionality

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const start = () => {
-  let numberArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
-
-  const shuffleArray = (array) => {
-    for (var i = numberArray.length - 1; i > 0; i--) {
-      var j = Math.floor(Math.random() * (i + 1));
-      var temp = numberArray[i];
-      array[i] = array[j];
-      array[j] = temp;
-    }
-  };
+  document.getElementById("startButton").removeAttribute("onclick");
 
   shuffleArray(numberArray);
   console.log(numberArray);
@@ -25,30 +16,53 @@ const start = () => {
   }
 };
 
-let userPicked1;
-let userPicked2;
+let numberArray = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6];
 
-let array = [];
+const shuffleArray = (array) => {
+  for (var i = numberArray.length - 1; i > 0; i--) {
+    var j = Math.floor(Math.random() * (i + 1));
+    var temp = numberArray[i];
+    array[i] = array[j];
+    array[j] = temp;
+  }
+};
+
+let userChosenCards = [];
 
 const userClicked = (e) => {
-  array.push(e.target.id);
-  console.log(array);
+  userChosenCards.push(e.target.id);
+  console.log(userChosenCards);
 
-  if (array.length === 2) {
+  if (userChosenCards.length === 2) {
     checkUserSelection();
   }
 };
 
-console.log(userPicked1);
-console.log(userPicked2);
+let correctGuesses = 0;
+let score = 0;
 
 const checkUserSelection = () => {
-  if (array[0] === array[1]) {
-    console.log("correct");
+  if (userChosenCards[0] === userChosenCards[1]) {
+    correctGuesses++;
+    console.log("correct" + correctGuesses);
+    document.getElementById(`${userChosenCards[0]}`).remove();
+    document.getElementById(`${userChosenCards[1]}`).remove();
+    // remove the correct cards
+    endGame();
   } else {
-    console.log("incorrect");
+    score++;
+    console.log("incorrect " + score);
   }
-  array = [];
+  userChosenCards = [];
 };
 
 let highScoresArray = [];
+
+const endGame = () => {
+  if (correctGuesses === 6) {
+    highScoresArray.push(score);
+    console.log("end game");
+    console.log(`${score} high scores`);
+    document.getElementById("replayButton").setAttribute("onclick", "start();");
+  }
+};
